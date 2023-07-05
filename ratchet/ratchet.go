01e5cde1f3546d6328095eb1f716aa41be3e6587
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2023-07-05 09:14:36 -0300

    First alpha version (#1)
    * Initial commit
    
    * ratchet: make provider address dynamic
    
    * test: make unitest phony
    
    * Replace gotestdox by go test
    
    * ratchet: wildcard make targets have odd dependency line requirements
    
    * ratchet: add tests via github actions
    
    ---------
    
    Co-authored-by: Jonathan Matthews <github@hello.jonathanmatthews.com>

---
+++ ratchet/ratchet.go
@@ -0,0 +1,152 @@
+package ratchet
+
+import (
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+
+	"github.com/tidwall/gjson"
+)
+
+func ValidTFSchemaVersion(schemaVersion gjson.Result) bool {
+	switch schemaVersion.Type {
+	case gjson.String:
+		return schemaVersion.String() == "1.0"
+	default:
+		return false
+	}
+}
+
+func EmitEntities(providerID string, JSONData []byte) string {
+	output := []string{}
+	gjson.GetBytes(JSONData, "provider_schemas").ForEach(func(key, value gjson.Result) bool {
+		if key.String() == providerID {
+			// providerID := key.String()
+			// fmt.Println("emit entities for " + providerID)
+			value.ForEach(func(key, value gjson.Result) bool {
+				if key.String() == "data_source_schemas" {
+					// fmt.Println("datasources:")
+					value.ForEach(func(key, value gjson.Result) bool {
+						output = append(output, fmt.Sprintf("%s: #DataSource: {", key.String()))
+						value.Get("block").Get("attributes").ForEach(func(key, value gjson.Result) bool {
+							if value.Get("computed").Bool() {
+								return true
+							}
+							tfType := value.Get("type")
+							if tfType.IsArray() {
+								if tfType.Array()[0].String() == "set" {
+									if tfType.Array()[1].IsArray() {
+										if tfType.Array()[1].Array()[0].String() == "object" {
+											output = append(output, fmt.Sprintf("    %s: [..._#%s]", key.String(), key.String()))
+										}
+										output = append(output, fmt.Sprintf("    _#%s: {", key.String()))
+										keys := []string{}
+										for k := range tfType.Array()[1].Array()[1].Map() {
+											keys = append(keys, k)
+										}
+										sort.Strings(keys)
+										for _, k := range keys {
+											output = append(output, fmt.Sprintf("        %s: %s", k, tfType.Array()[1].Array()[1].Map()[k]))
+										}
+										output = append(output, "    }")
+									}
+								}
+								return true
+							}
+							//fmt.Println(tfType.Type, tfType.Raw, tfType.String())
+							CUEType, err := ConvertTerraformType(tfType)
+							if err != nil {
+								log.Fatal(err)
+							}
+							switch {
+							case value.Get("required").Bool():
+								output = append(output, fmt.Sprintf("    %s!: %s", key.String(), CUEType))
+							case value.Get("optional").Bool():
+								output = append(output, fmt.Sprintf("    %s?: %s", key.String(), CUEType))
+							default:
+								output = append(output, fmt.Sprintf("    %s: %s", key.String(), CUEType))
+							}
+							return true
+						})
+						output = append(output, "}")
+						return true
+					})
+				}
+				if key.String() == "resource_schemas" {
+					// fmt.Println("datasources:")
+					value.ForEach(func(key, value gjson.Result) bool {
+						output = append(output, fmt.Sprintf("%s: #Resource: {", key.String()))
+						value.Get("block").Get("attributes").ForEach(func(key, value gjson.Result) bool {
+							if value.Get("computed").Bool() {
+								return true
+							}
+							tfType := value.Get("type")
+							if tfType.IsArray() {
+								if tfType.Array()[0].String() == "set" {
+									if tfType.Array()[1].IsArray() {
+										if tfType.Array()[1].Array()[0].String() == "object" {
+											output = append(output, fmt.Sprintf("    %s: [..._#%s]", key.String(), key.String()))
+										}
+										output = append(output, fmt.Sprintf("    _#%s: {", key.String()))
+										for k, v := range tfType.Array()[1].Array()[1].Map() {
+											output = append(output, fmt.Sprintf("        %s: %s", k, v))
+										}
+										output = append(output, "    }")
+									}
+								}
+								return true
+							}
+							CUEType, err := ConvertTerraformType(tfType)
+							if err != nil {
+								log.Fatal(err)
+							}
+							switch {
+							case value.Get("required").Bool():
+								output = append(output, fmt.Sprintf("    %s!: %s", key.String(), CUEType))
+							case value.Get("optional").Bool():
+								output = append(output, fmt.Sprintf("    %s?: %s", key.String(), CUEType))
+							default:
+								output = append(output, fmt.Sprintf("    %s: %s", key.String(), CUEType))
+							}
+							return true
+						})
+						output = append(output, "}")
+						return true
+					})
+				}
+				return true
+			})
+		}
+		return true
+	})
+	return strings.Join(output, "\n")
+}
+
+func ConvertTerraformType(TFType gjson.Result) (string, error) {
+	switch TFType.Type {
+	case gjson.String:
+		return TFType.String(), nil
+	case gjson.JSON:
+		if TFType.IsArray() {
+			TFTypeItems := TFType.Array()
+			if len(TFTypeItems) != 2 {
+				return "", fmt.Errorf("%d is invalid number of items. Expecting 2", len(TFTypeItems))
+			}
+			switch TFTypeItems[0].String() {
+			case "list", "set":
+				if !TFTypeItems[1].IsArray() {
+					return fmt.Sprintf("[...%s]", TFTypeItems[1].String()), nil
+				}
+				return "", fmt.Errorf("i dont know what to return")
+			case "map":
+				return fmt.Sprintf("[string]: %s", TFTypeItems[1].String()), nil
+			default:
+				return "", fmt.Errorf("not sure what to do with %q", TFTypeItems[0].String())
+			}
+		}
+		return "", fmt.Errorf("i dont know what to return")
+	default:
+		return "", fmt.Errorf("unkown type %q", TFType.Type)
+	}
+}
---
+++ ratchet/ratchet_test.go
@@ -0,0 +1,457 @@
+package ratchet_test
+
+import (
+	"testing"
+
+	"github.com/cueniform/internal-tools/ratchet"
+	"github.com/google/go-cmp/cmp"
+	"github.com/tidwall/gjson"
+)
+
+func TestValidTFSChemaVersion_ReturnsTrueGivenKnownVersion(t *testing.T) {
+	t.Parallel()
+	want := true
+	input := gjson.Result{
+		Type: gjson.String,
+		Raw:  "1.0",
+		Str:  "1.0",
+	}
+	got := ratchet.ValidTFSchemaVersion(input)
+	if want != got {
+		t.Fatalf("want valid %t, got %t", want, got)
+	}
+}
+
+func TestValidTFSChemaVersion_ReturnsFalseGivenNumber(t *testing.T) {
+	t.Parallel()
+	want := false
+	input := gjson.Result{
+		Type: gjson.Number,
+	}
+	got := ratchet.ValidTFSchemaVersion(input)
+	if want != got {
+		t.Fatalf("want valid %t, got %t", want, got)
+	}
+}
+
+func TestValidTFSChemaVersion_ReturnsFalseGivenFalse(t *testing.T) {
+	t.Parallel()
+	want := false
+	input := gjson.Result{
+		Type: gjson.False,
+	}
+	got := ratchet.ValidTFSchemaVersion(input)
+	if want != got {
+		t.Fatalf("want valid %t, got %t", want, got)
+	}
+}
+
+func TestValidTFSChemaVersion_ReturnsFalseGivenTrue(t *testing.T) {
+	t.Parallel()
+	want := false
+	input := gjson.Result{
+		Type: gjson.True,
+	}
+	got := ratchet.ValidTFSchemaVersion(input)
+	if want != got {
+		t.Fatalf("want valid %t, got %t", want, got)
+	}
+}
+
+func TestValidTFSChemaVersion_ReturnsFalseGivenNull(t *testing.T) {
+	t.Parallel()
+	want := false
+	input := gjson.Result{
+		Type: gjson.Null,
+	}
+	got := ratchet.ValidTFSchemaVersion(input)
+	if want != got {
+		t.Fatalf("want valid %t, got %t", want, got)
+	}
+}
+
+func TestValidTFSChemaVersion_ReturnsFalseGivenJSON(t *testing.T) {
+	t.Parallel()
+	want := false
+	input := gjson.Result{
+		Type: gjson.JSON,
+	}
+	got := ratchet.ValidTFSchemaVersion(input)
+	if want != got {
+		t.Fatalf("want valid %t, got %t", want, got)
+	}
+}
+
+func TestValidTFSChemaVersion_ReturnsFalseGivenUnkownType(t *testing.T) {
+	t.Parallel()
+	want := false
+	input := gjson.Result{
+		Type: 666,
+	}
+	got := ratchet.ValidTFSchemaVersion(input)
+	if want != got {
+		t.Fatalf("want valid %t, got %t", want, got)
+	}
+}
+
+func TestConvertTerraformType_ReturnsExpectedStringGivenString(t *testing.T) {
+	t.Parallel()
+	want := "string"
+	got, err := ratchet.ConvertTerraformType(gjson.Result{
+		Type: gjson.String,
+		Raw:  `"string"`,
+		Str:  "string",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %q, got %q", want, got)
+	}
+}
+
+func TestConvertTerraformType_ReturnsExpectedStringGivenNumber(t *testing.T) {
+	t.Parallel()
+	want := "number"
+	got, err := ratchet.ConvertTerraformType(gjson.Result{
+		Type: gjson.String,
+		Raw:  `"number"`,
+		Str:  "number",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %q, got %q", want, got)
+	}
+}
+
+func TestConvertTerraformType_ReturnsExpectedStringGivenListString(t *testing.T) {
+	t.Parallel()
+	want := "[...string]"
+	got, err := ratchet.ConvertTerraformType(gjson.Result{
+		Type: gjson.JSON,
+		Raw:  `["list", "string"]`,
+		Str:  `["list", "string"]`,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %q, got %q", want, got)
+	}
+}
+
+func TestConvertTerraformType_ReturnsExpectedStringGivenSetString(t *testing.T) {
+	t.Parallel()
+	want := "[...string]"
+	got, err := ratchet.ConvertTerraformType(gjson.Result{
+		Type: gjson.JSON,
+		Raw:  `["set", "string"]`,
+		Str:  `["set", "string"]`,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %q, got %q", want, got)
+	}
+}
+
+func TestConvertTerraformType_ReturnsExpectedStringGivenListBool(t *testing.T) {
+	t.Parallel()
+	want := "[...bool]"
+	got, err := ratchet.ConvertTerraformType(gjson.Result{
+		Type: gjson.JSON,
+		Raw:  `["list", "bool"]`,
+		Str:  `["list", "bool"]`,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %q, got %q", want, got)
+	}
+}
+
+func TestConvertTerraformType_ErrorsGivenListWithThreeItems(t *testing.T) {
+	t.Parallel()
+	_, err := ratchet.ConvertTerraformType(gjson.Result{
+		Type: gjson.JSON,
+		Raw:  `["a", "b", "c"]`,
+		Str:  `["a", "b", "c"]`,
+	})
+	if err == nil {
+		t.Fatal("want error but got nil")
+	}
+}
+
+func TestEmitEntities_ReturnsExpectedStringGivenDataSourceWithBasicAttribute(t *testing.T) {
+	t.Parallel()
+	want := `bogus: #DataSource: {
+    bogus: string
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "data_source_schemas": {
+        "bogus": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "bogus": {
+                "type": "string"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestEmitEntities_SkipsItemsWithComputedTrue(t *testing.T) {
+	t.Parallel()
+	want := `bogus: #DataSource: {
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "data_source_schemas": {
+        "bogus": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "bogus": {
+                "type": "string"
+				"computed": true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestEmitEntities_ReturnsExpectedStringGivenDataSourceWithRequiredAttribute(t *testing.T) {
+	t.Parallel()
+	want := `bogus: #DataSource: {
+    bogus!: string
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "data_source_schemas": {
+        "bogus": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "bogus": {
+                "type": "string",
+				"required": true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestEmitEntities_ReturnsExpectedStringGivenDataSourceWithOptionalAttribute(t *testing.T) {
+	t.Parallel()
+	want := `bogus: #DataSource: {
+    bogus?: string
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "data_source_schemas": {
+        "bogus": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "bogus": {
+                "type": "string",
+				"optional": true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestEmitEntities_ReturnsExpectedStringGivenResource(t *testing.T) {
+	t.Parallel()
+	want := `bogus: #Resource: {
+    bogus: string
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "resource_schemas": {
+        "bogus": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "bogus": {
+                "type": "string"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestEmitEntities_ReturnsExpectedEntityGivenDataSourceWithTwoAttributes(t *testing.T) {
+	t.Parallel()
+	want := `bogus: #DataSource: {
+    foo: string
+    bar: number
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "data_source_schemas": {
+        "bogus": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "foo": {
+                "type": "string"
+              },
+			  "bar": {
+                "type": "number"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestEmitEntities_ReturnsExpectedEntitiesGivenTwoDataSources(t *testing.T) {
+	t.Parallel()
+	want := `data_source1: #DataSource: {
+    bogus: string
+}
+data_source2: #DataSource: {
+    bogus: string
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "data_source_schemas": {
+        "data_source1": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "bogus": {
+                "type": "string"
+              }
+            }
+          }
+        },
+		"data_source2": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "bogus": {
+                "type": "string"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestEmitEntities_ReturnsExpectedEntityGivenDataSourceWithObject(t *testing.T) {
+	t.Parallel()
+	want := `bogus: #DataSource: {
+    myComplexObj: [..._#myComplexObj]
+    _#myComplexObj: {
+        field1: string
+        field2: string
+    }
+}`
+	inputJSON := []byte(`{
+  "format_version": "1.0",
+  "provider_schemas": {
+    "bogus": {
+      "data_source_schemas": {
+        "bogus": {
+          "version": 0,
+          "block": {
+            "attributes": {
+              "myComplexObj": {
+                "type": ["set",[
+                    "object",
+                    {
+                      "field1": "string",
+                      "field2": "string"
+                    }
+                  ]
+				]
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+	got := ratchet.EmitEntities("bogus", inputJSON)
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
